211e55c067e35f9ca4290e0e86d7e93e8c8991d4
Yang LIU <liuyangcharlie@gmail.com>
2019-12-14 17:17:01 -0500

    WIP: use RPyC instead of Thrift for RPC

--- node.py
+++ node.py
@@ -7,42 +7,67 @@ import threading
 from env import *
 from address import inrange
 from finger_entry import FingerEntry
+from rpc_server import RPC
+from rpyc.utils.server import ThreadedServer
 
 # class representing a local peer
 class Node(object):
-  def __init__(self, local_address, remote, remote_address = None):
+  def __init__(self, local_address, remote_address = None):
     self._address = local_address
-    _id = self._address.__hash__() % NUM_SLOTS
-    while remote.getRemoteNodeByID(_id) is not None:
-      _id = (_id + 1) % NUM_SLOTS
-    self._id = _id
 
-    # communication with other node via _remote
-    self._remote = remote
+    # get identifier
+    _id = self._address.__hash__() % NUM_SLOTS
+
+    """
+    avoid collision
+    TODO: find through remote_node to see if other node has the same id
+    """
+    # while remote.getRemoteNodeByID(_id) is not None:
+    #   _id = (_id + 1) % NUM_SLOTS
+    # self._id = _id
+
     # initialize successor
     self._successor = None
+
     # list of successors is to prevent lookup failure
     self._successors = [None for x in range(M_BIT)]
+
     # initialize predecessor
     self._predecessor = None
+
     # finger table
     self._finger = None
     self._leave = False
 
-    self._remote.addToNetwork(self._id, self)
+    # a hash map of rpc client to remote nodes, key is ip, value is the client
+    self.remote_clients = {}
+
+    # means that node in on the ring
+    # TODO: to be removed when using RPC, DEPRECATED
+    # self._remote.addToNetwork(self._id, self)
 
     # join the DHT
     self.join(remote_address)
 
     # in case any node depatures
-    self.check_predecessor()
+    # self.check_predecessor()
+
+    # initilize RPC server
+    thread = ThreadedServer(RPC(self), hostname='0.0.0.0', port = 18861)
+    self._thread = threading.Thread(target=thread.start)
+    self._thread.start()
+    print('RPC server started...')
+
 
   def address(self):
     return self._address.__str__()
 
+
   # node leave
   def leave(self):
     self._leave = True
+    # exit(0)
+
 
   # logging function
   def log(self, info):
@@ -51,16 +76,24 @@ class Node(object):
       f.close()
       print(str(self.id()) + " : " +  info)
 
+
   # return true if node does not leave, i.e. still in the Chord ring
   def ping(self):
     if self._leave:
       return False
     return True
 
-  # find the exact successor by comparing the hash(n), can be regarded as a lookup
-  # 1. initialize the predecessor and the finger table
-  # 2. notify other nodes to update their predecessors and finger tables
-  # 3. the new node takes over its responsible keys from its successor.
+
+  def get_remote_node(self, ip):
+    #   pass
+
+
+  """
+  find the exact successor by comparing the hash(n), can be regarded as a lookup
+  1. initialize the predecessor and the finger table
+  2. notify other nodes to update their predecessors and finger tables
+  3. the new node takes over its responsible keys from its successor.
+  """
   def join(self, remote_address = None):
     # initialize finger table
     self._finger = [None for x in range(M_BIT)]
@@ -68,13 +101,23 @@ class Node(object):
     if remote_address:
       # 1) add to a node `n`, n.find_successor(`to_be_added`)
       start = (self.id() + (2 ** 0)) % NUM_SLOTS
-      remote_node = self._remote.getRemoteNode(remote_address)
-      successor = remote_node.find_successor(start)
+      # TODO: replace _remote.getRemoteNode method, start a RPC client instead
+      # remote_node = self._remote.getRemoteNode(remote_address)
+      # find rpc client to remote node
+
+
+      # TODO: RPC call find_successor
+      # successor = remote_node.find_successor(start)
+
+
       self._finger[0] = FingerEntry(start, successor)
+
       # 2) point `to_be_added`’s `successor` to the node found
       self._successor = successor
+
       # 3) copy keys less than `ID(to_be_added)` from the `successor`
       self._predecessor = successor._predecessor
+
       # update its successor's predecessor
       self._successor._predecessor = self
 
@@ -91,45 +134,58 @@ class Node(object):
 
     self.update_successors()
 
-    # 4) call `to_be_added`.stabilize() to update the nodes between `to_be_added` and its predecessor
+    # # 4) call `to_be_added`.stabilize() to update the nodes between `to_be_added` and its predecessor
     self.stabilize()
 
     self.log("joined")
 
-  # first node on circle that succeeds (n + 2^k−1) mod 2m, 1 <= k <= m
-  # i-th entry means the 2^i far-away node from the current node
+# ---------------------------------------------
+
+  """
+  first node on circle that succeeds (n + 2^k−1) mod 2m, 1 <= k <= m
+  i-th entry means the 2^i far-away node from the current node
+  """
   def init_finger(self, remote_address = None):
     if remote_address:
       # get the arbitrary node in which the target node want to join
       remote_node = self._remote.getRemoteNode(remote_address)
 
-      # successor
+      # first find its successor, i.e. the first entry in its finger table
       successor = self.successor()
       if successor is None:
+        # TODO: replace with RPC call find_succ
         successor = remote_node.find_successor(self.id())
         self._successor = successor
 
-      # initialize finger table
+      # initialize the rest of its finger table
       for x in range(1, M_BIT):
         start_id = (self.id() + 2 ** x) % NUM_SLOTS
         self._finger[x] = FingerEntry(start_id, None)
 
+      # find the corresponding nodes that are supposed to be in the finger table
       for x in range(0, M_BIT - 1):
         start_id = self._finger[x + 1].start
+
         if inrange(start_id, self.id(), self._finger[x].node.id()):
+          # if inrange, no RPC call needed, assign locally
           self._finger[x + 1].node = self._finger[x].node
         else:
-          # 
+          """
+          need to call find successor leveraging finger table
+          for `self.find_successor`, if its the first node
+          """
           successor = self.find_successor(start_id)
           self._finger[x + 1] = FingerEntry(start_id, successor)
 
     else:
-    # n is the only node in the network
+      # n is the only node in the network
       for x in range(0, M_BIT):
         start_id = math.floor((self.id() + 2 ** x) % NUM_SLOTS)
         self._finger[x] = FingerEntry(start_id, self)
 
     self.print_finger('init_finger')
+
+# ---------------------------------------------
 
   # called periodically
   # back-up successor list, a M_BIT-long successor link list
@@ -149,6 +205,8 @@ class Node(object):
   def id(self, offset = 0):
     return self._id
 
+
+  # for successor other than the node itself, `successor` returns the Netrefs instance of a remote node
   def successor(self):
     successor = self._successor
     print('current successor', self._successor.ping())
@@ -162,15 +220,22 @@ class Node(object):
 
     return successor
 
+
+  # for predecessor other than the node itself, `predecessor` returns the Netrefs instance of a remote node
   def predecessor(self):
     return self._predecessor
 
+
+  # TODO: one thing to note is this may return a Netrefs instance of a remote node, rather than a Node instance
   def find_successor(self, id):
     self.log("find_successor of {}".format(id))
     # if self._predecessor exists, and _predecessor.id < id < self.id, the successor is current node
     if self._predecessor and inrange(id, self._predecessor.id(), self.id()):
       return self
+
+    # TODO: replace `find_predecessor` and `successor` with RPC call
     return self.find_predecessor(id).successor()
+
 
   def find_predecessor(self, id):
     lg = "find_predecessor of: {}".format(id)
@@ -184,15 +249,19 @@ class Node(object):
       node = node._closest_preceding_node(id)
     return node
 
+
   def _closest_preceding_node(self, id):
     # from m down to 1
     for x in reversed(range(len(self._finger))):
       entry = self._finger[x]
+      # TODO: replace id method with RPC call, maybe a new method to replace _closest_preceding_node
       if entry != None and entry.node != None and inrange(entry.node.id(), self.id(), id):
         return entry.node
 
     return self
 
+
+  # used for network visualization application
   def get_finger(self):
     finger = []
     for x in range(len(self._finger)):
@@ -203,24 +272,36 @@ class Node(object):
 
     return finger
 
+
   def update_finger(self, successor, index):
     if self._finger[index] is not None:
       if inrange(successor.id(), self.id() - 1, self._finger[index].node.id()):
         self._finger[index].node = successor
+        # TODO: replace `update_finger` with a RPC call
         self._predecessor.update_finger(successor, index)
         # print('finger table of ', self.id(), 'start: ', self._finger[x].start, 'node', self._finger[x].node.id())
 
     # threading.Timer(2, self.update_finger).start()
 
+
+  # DEPRECATED
   def update_others(self):
     for x in range(1, M_BIT + 1):
       # find last node whose i-th finger might be current node
       start = (self.id() - 2 ** (x - 1)) % NUM_SLOTS
+
+      """
+      2 cases for such invocations:
+      - returns a Node instance when only one node in the network
+      - returns a Netref instance of a remote node
+      """
       pre = self.find_predecessor(start)
+
       # if only one node on the ring, no need to update others
       if pre.id() == self.id():
         continue
       pre.update_finger(self, x)
+
 
   # called periodically
   # clear the node’s predecessor pointer if n.predecessor is alive, or has failed
@@ -234,6 +315,7 @@ class Node(object):
       self._predecessor = None
 
     threading.Timer(2, self.check_predecessor).start()
+
 
   # called periodically
   # check its own successor if any new node added between its previous successor
@@ -253,12 +335,14 @@ class Node(object):
 
     threading.Timer(2, self.stabilize).start()
 
+
   # RPC call
   # receive request that some node thinks it might be our predecessor
   def notify(self, pre):
     # check if pre is the new predecessor
     if (self._predecessor is None or inrange(pre.id(), self._predecessor.id(), self.id())):
       self._predecessor = pre
+
 
   # called periodically
   # randomly update finger table
@@ -272,10 +356,12 @@ class Node(object):
 
     threading.Timer(2, self.fix_finger).start()
 
+
   # update both first entry in finger table and _successor
   def update_successor(self, new_s):
     self._successor = new_s
     self._finger[0].node = new_s
+
 
   def print_finger(self, mod='default'):
     for x in range(0, M_BIT):
