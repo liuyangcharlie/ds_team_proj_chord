a459dc15f407fd295605dc13ed5fd34625728dc2
Yang LIU <liuyangcharlie@gmail.com>
2019-12-16 17:54:07 -0500

    change port binding, change app server addr and port

--- chordsite/util.py
+++ chordsite/util.py
@@ -1,4 +1,5 @@
 import socket
+import rpyc
 
 def local_ip():
   return socket.gethostbyname(socket.gethostname())
@@ -10,11 +11,22 @@ def is_remote_node(node):
     pass
 
 def ringShape(node):
+    # read file node_addr to get add ips
+    # TODO: we may be able to replace it with database
+    s = []
+    f= open("./node_arr","r")
+    for ip in f:
+        print('------------------------')
+        print(ip)
+
+
     s = []
     for x in range(len(node._nodes)):
       if node._nodes[x] is not None:
         s.append({'addr': node._nodes[x]._address.__str__(), 'finger': node._nodes[x].get_finger()})
       else:
         s.append(None)
+
+    f.close()
 
     return s
